08d059aa32389127306b45fe0deaa51667e16e41
James A Ridley <jrid88@gmail.com>
2023-01-29 17:02:25 -0500

    Made some changes in models.py to try and fix the issue of adding users to the database.

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
-app = Flask(__name__)
+app = Flask(__name__, instance_path='/Users/jaridley/PycharmProjects/BlogPost/flaskblog/')
 
 app.config['SECRET_KEY'] = '6a64a9d92a7ded6e5e72eca0c748532d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
@@ -10,5 +11,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
